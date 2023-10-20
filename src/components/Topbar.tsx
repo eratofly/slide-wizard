2e547e30f127b520743e9961b18c37e096cf19f1
Daria Okhotnikova <d_okhotnikova@mail.ru>
2023-10-20 10:53:15 +0300

    SW-2 добавление компонентов

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Topbar.css'
+
+function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbar__titlebar">
+                <TopBarButton title="File" />
+                <PresentationTitle name="Enter presentation title" />
+                <img src="" alt="" />
+            </div>
+            <ToolBar />
+        </div>
+    )
+}
+
+export default Topbar
