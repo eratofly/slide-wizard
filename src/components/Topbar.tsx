3180aa9ee5d9b62db73dfcf7ae363c3cf93329ab
AlinaDmitrieva12 <alina.dmitrievaa12@gmail.com>
2023-11-03 00:42:19 +0300

    SW-3 Тестовый коммит

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './Topbar.css'
 
 function Topbar() {
-    return (
-        <div className="topbar">
-            <div className="topbar__titlebar">
-                <TopBarButton title="File" />
-                <PresentationTitle name="Enter presentation title" />
-                <img src="" alt="" />
-            </div>
-            <ToolBar />
-        </div>
-    )
+	return (
+		<div className="topbar">
+			<div className="topbar__titlebar">
+				{/*<TopBarButton title="File" />*/}
+				{/*<PresentationTitle name="Enter presentation title" />*/}
+				<img src="" alt="" />
+			</div>
+			{/*<ToolBar />*/}
+		</div>
+	)
 }
 
 export default Topbar
--- src/components/editorView.tsx
+++ src/components/editorView.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Editor } from '../model/types'
 import { SlidesPreview } from './slidePreview'
+import Topbar from './Topbar'
 
 type EditorViewProps = {
 	editor: Editor
@@ -11,6 +12,7 @@ function EditorView(props: EditorViewProps) {
 		<div>
 			<div className="top-bar"></div>
 			<div className="working-field">
+				<Topbar />
 				<SlidesPreview slides={props.editor.presentation.slides} />
 			</div>
 		</div>
