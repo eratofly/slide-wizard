99178f360c3917519a1ff06d5ae1c12f67da9b6f
Daria Okhotnikova <d_okhotnikova@mail.ru>
2023-11-24 10:34:12 +0300

    SW-2 картинки

---
+++ src/components/button/icons.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+
+const deleteSlideBtn = (
+	<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_201_3)">
+			<path
+				d="M14.3201 1C15.4721 1 15.9201 3.02667 16.0001 4.04H5.6001C5.6001 1.608 6.66676 1 7.2001 1H14.3201Z"
+				stroke="#343434"
+				stroke-width="1.36"
+			/>
+			<rect y="3.23999" width="21.6" height="1.6" rx="0.8" fill="#343434" />
+			<rect
+				x="8.95996"
+				y="6.12"
+				width="11.04"
+				height="1.6"
+				rx="0.8"
+				transform="rotate(90 8.95996 6.12)"
+				fill="#343434"
+			/>
+			<rect
+				x="14.0801"
+				y="6.12"
+				width="11.04"
+				height="1.6"
+				rx="0.8"
+				transform="rotate(90 14.0801 6.12)"
+				fill="#343434"
+			/>
+			<rect
+				x="19.04"
+				y="6.12"
+				width="11.84"
+				height="1.6"
+				rx="0.8"
+				transform="rotate(90 19.04 6.12)"
+				fill="#343434"
+			/>
+			<path
+				d="M3.2002 17.4C3.22686 18.6 3.8882 21 6.3202 21H15.2002C16.2135 21 18.2402 20.04 18.2402 17.16"
+				stroke="#343434"
+				stroke-width="1.36"
+			/>
+			<rect
+				x="4"
+				y="6.12"
+				width="12.16"
+				height="1.6"
+				rx="0.8"
+				transform="rotate(90 4 6.12)"
+				fill="#343434"
+			/>
+		</g>
+		<defs>
+			<clipPath id="clip0_201_3">
+				<rect width="22" height="22" fill="white" />
+			</clipPath>
+		</defs>
+	</svg>
+)
+
+const newSlideBtn = (
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_201_2)">
+			<rect
+				x="11.1113"
+				width="20"
+				height="2.22222"
+				rx="1.11111"
+				transform="rotate(90 11.1113 0)"
+				fill="#343434"
+			/>
+			<rect y="8.88889" width="20" height="2.22222" rx="1.11111" fill="#343434" />
+		</g>
+		<defs>
+			<clipPath id="clip0_201_2">
+				<rect width="20" height="20" fill="white" />
+			</clipPath>
+		</defs>
+	</svg>
+)
+
+const redoBtn = (
+	<svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_202_5)">
+			<path
+				d="M13.0867 17H7.42001C5.61325 17 1.99972 15.7681 1.99972 10.8406C1.99972 9.03381 3.08378 5.42029 7.42001 5.42029H14.5649"
+				stroke="#343434"
+				stroke-width="2.46377"
+			/>
+			<path
+				d="M21.71 5.42032L13.5795 0.726209L13.5795 10.1144L21.71 5.42032Z"
+				fill="#343434"
+			/>
+		</g>
+		<defs>
+			<clipPath id="clip0_202_5">
+				<rect width="22" height="19" fill="white" />
+			</clipPath>
+		</defs>
+	</svg>
+)
+
+const undoSvg = (
+	<svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M8.62328 17H14.2899C16.0967 17 19.7102 15.7681 19.7102 10.8406C19.7102 9.03381 18.6262 5.42029 14.2899 5.42029H7.14502"
+			stroke="#343434"
+			stroke-width="2.46377"
+		/>
+		<path
+			d="M-2.36928e-07 5.42032L8.13043 0.726209L8.13043 10.1144L-2.36928e-07 5.42032Z"
+			fill="#343434"
+		/>
+	</svg>
+)
+
+const borderColorPrimitiveBtn = (
+	<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="6.0293" y="22.8311" width="26.0174" height="10.095" transform="rotate(-45 6.0293 22.8311)" fill="black"/>
+		<path d="M5.98128 22.7379L13.2581 30.0177L2.89823 32.5584L5.98128 22.7379Z" fill="black"/>
+		<path d="M8.15926 24.6266L11.6161 28.0849L6.69467 29.2918L8.15926 24.6266Z" fill="white"/>
+		<rect x="8.31738" y="24.8533" width="22.8041" height="4.29038" transform="rotate(-45 8.31738 24.8533)" fill="white"/>
+		<path d="M24.9932 3.86963L26.7415 2.1213C27.9131 0.949722 29.8126 0.949723 30.9841 2.1213L33.8784 5.01555C35.05 6.18713 35.05 8.08662 33.8784 9.25819L32.1301 11.0065L24.9932 3.86963Z" fill="black"/>
+	</svg>
+)
+
+const borderThicknessPrimitiveBtn = (
+	<svg width="32" height="26" viewBox="0 0 32 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="0.660156" width="30.4227" height="3.14989" fill="black"/>
+		<rect x="0.660156" y="6.40479" width="30.4227" height="2.56191" fill="black"/>
+		<rect x="0.660156" y="12.8096" width="30.4227" height="1.92144" fill="black"/>
+		<rect x="0.660156" y="19.2144" width="30.4227" height="1.28096" fill="black"/>
+		<rect x="0.660156" y="25.1995" width="30.4227" height="0.800598" fill="black"/>
+	</svg>
+)
+
