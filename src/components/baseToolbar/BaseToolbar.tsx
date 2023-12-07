0ab46af1d3a95c945fbf4297d1ad9c23ddb171b4
AlinaDmitrieva12 <alina.dmitrievaa12@gmail.com>
2023-12-08 01:20:10 +0300

    alina-lw6 добавила поповер для примитивов

--- src/components/baseToolbar/BaseToolbar.tsx
+++ src/components/baseToolbar/BaseToolbar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Button } from '../button/Button'
 import styles from './BaseToolbar.module.css'
 import { newSlideBtn, deleteSlideBtn, undoBtn, redoBtn } from '../button/icons'
+import { FigurePicker, FigurePickerItem } from '../figurePicker/FigurePicker'
+import { RectIcon } from './res/RectIcon'
+import { EllipseIcon } from './res/EllipseIcon'
+import { TriangleIcon } from './res/TriangleIcon'
+import { useClickOutside } from '../../hooks/useOutsideClick'
 
 export function BaseToolbar() {
+	const figurePickerItems: FigurePickerItem[] = [
+		{
+			id: 'rect',
+			icon: <RectIcon />,
+			onClick: () => {
+				console.log('add rectangle')
+			},
+		},
+		{
+			id: 'ellipse',
+			icon: <EllipseIcon />,
+			onClick: () => {
+				console.log('add ellipse')
+			},
+		},
+		{
+			id: 'triangle',
+			icon: <TriangleIcon />,
+			onClick: () => {
+				console.log('add triangle')
+			},
+		},
+	]
+
+	const [figurePickerOpened, setFigurePickerOpened] = useState(false)
+	const figurePickerRef = useRef(null)
+	useClickOutside(figurePickerRef, () => setFigurePickerOpened(false))
+
 	return (
 		<div className={styles.baseToolbar}>
 			<div className={styles.addDeleteBtn}>
@@ -17,8 +50,19 @@ export function BaseToolbar() {
 			<div className={styles.redactorBtn}>
 				<Button text="Text" typeButton="default" />
 				<Button text="Image" typeButton="default" />
-				<Button text="Primitive" typeButton="default" />
+				<Button
+					text="Primitive"
+					typeButton="default"
+					onClick={() => {
+						if (!figurePickerOpened) {
+							setFigurePickerOpened(true)
+						}
+					}}
+				/>
 				<Button text="Background" typeButton="default" />
+			</div>
+			<div ref={figurePickerRef}>
+				{figurePickerOpened && <FigurePicker items={figurePickerItems} />}
 			</div>
 		</div>
 	)
---
+++ src/components/figurePicker/FigurePicker.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import styles from './FigurePicker.module.css'
+import { ReactElement, RefObject } from 'react'
+
+type FigurePickerItem = {
+	id: string
+	icon: ReactElement
+	onClick: () => void
+}
+
+type FigurePickerProps = {
+	items: FigurePickerItem[]
+}
+
+function FigurePicker(props: FigurePickerProps) {
+	return (
+		<div className={styles.container}>
+			{props.items.map((item) => (
+				<div key={item.id} className={styles.item} onClick={item.onClick}>
+					{item.icon}
+				</div>
+			))}
+		</div>
+	)
+}
+
+export { FigurePicker, type FigurePickerItem }
